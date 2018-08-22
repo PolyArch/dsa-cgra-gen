b47c1b1024d64c0d1661ac7dc15fb0e4520c6c24
SihaoLiu <liusihao.china@gmail.com>
2018-08-21 22:29:53 -0700

    Switch created

--- src/main/scala/cgra/switch.scala
+++ src/main/scala/cgra/switch.scala
@@ -3,8 +3,30 @@ package cgraexplorationframework.simplechip.cgra
 import chisel3._
 import cgraexplorationframework.simplechip.tile._
 import cgraexplorationframework.simplechip.config.Parameters
+import chisel3.util._
 
-class Switch(implicit p:Parameters) extends FabricModule
-  with HasFabricModuleParams{
-  val io = new class extends Bundle
+class Switch(
+              numInput        : Int,
+              numOutput       : Int,
+              inputDirection  : Array[Double],
+              outputDirection : Array[Double],
+              decomp          : Int
+            ) extends FabricModule //with FabricModuleIO
+{
+
+  override val datawidthModule: Int = CGRAdataWidth
+  override val numModuleInput:Int = numInput
+  override val numModuleOutput: Int = numOutput
+  override val inputMoudleDirection: Array[Double] = inputDirection
+  override val outputModuleDirection: Array[Double] = outputDirection
+  override lazy val numDecomp: Int = decomp
+
+  io.output_ports(0).bits := io.input_ports(0).bits
+  io.input_ports(0).ready := false.B
+  io.output_ports(0).valid := false.B
+
+}
+
+object SwitchDriver extends App {
+  chisel3.Driver.execute(args, () => new Switch(1,1,Array(0.0),Array(0.0),2))
 }
--- src/main/scala/tile/fabric.scala
+++ src/main/scala/tile/fabric.scala
@@ -8,24 +8,44 @@ trait HasFabricParams {
   val CGRAdataWidth = 64
 }
 
-trait HasFabricModuleParams with HasFabricParams{
-  val numInput : Int
-  val numOutput : Int
-  val inputDirection : Array[Double]
-  val outputDirection : Array[Double]
+trait HasFabricModuleParams extends HasFabricParams
+{
+  val datawidthModule       : Int
+  val numModuleInput        : Int
+  val numModuleOutput       : Int
+  val inputMoudleDirection  : Array[Double]
+  val outputModuleDirection : Array[Double]
+  val numDecomp             : Int
 
-  val test : UInt
-
-  val numDecomp : Int
   require(isPow2(CGRAdataWidth/numDecomp))
 }
 
-trait HasFabricModuleIO
 
-abstract class FabricModuleBundle(implicit p: Parameters) extends Bundle
-  with FabricModuleParams
+abstract class FabricModule  extends Module
+  with HasFabricModuleParams{
+  lazy val io = IO(new Bundle{
+    val input_ports = Vec(numModuleInput,Flipped(DecoupledIO(UInt(datawidthModule.W))))
+    val output_ports = Vec(numModuleOutput,DecoupledIO(UInt(datawidthModule.W)))
+  })
+}
 
+/*
+trait HasFabricModuleIO extends FabricModuleBundle
+  with HasFabricModuleParams{
 
+  implicit val p: Parameters
+  val io = new FabricModuleBundle()(p) {
+    val input_ports = Flipped(DecoupledIO(UInt(datawidthModule.W)))
+  }
+}
+*/
 
-abstract class FabricModule (implicit val p: Parameters) extends Module
-  with FabricModuleParams
+/*
+trait FabricModuleIO extends Bundle
+  with HasFabricModuleParams{
+  val io = new Bundle{
+    val input_ports = Vec(numModuleInput,Flipped(DecoupledIO(UInt(datawidthModule.W))))
+    val output_ports = Vec(numModuleOutput,DecoupledIO(UInt(datawidthModule.W)))
+  }
+}
+*/
