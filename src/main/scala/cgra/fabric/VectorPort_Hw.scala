fdccf96c516002131323377a566bfa4da0b91186
SihaoLiu <liusihao.china@gmail.com>
2019-03-25 22:11:53 -0700

    add fifo buffer in vector port

--- src/main/scala/cgra/fabric/VectorPort_Hw.scala
+++ src/main/scala/cgra/fabric/VectorPort_Hw.scala
@@ -24,22 +24,38 @@ class VectorPort_Hw(name_p:(String,vector_port)) extends Module
 
   if(num_vector > 1){
     val distribute_counter = RegInit(0.U(log2Ceil(num_vector).W))
-    val vector_registers = Reg(Vec(num_vector,ReqAckConf_t(data_word_width)))
-
     if (num_output >= num_input){
-      for (i <- 0 until num_vector)
-        io.out(i) <>  vector_registers(i)
-      vector_registers(distribute_counter) <> io.in.head
+      for (i <- 0 until num_vector){
+        val io_in_decoup = Wire(DecoupledIO(UInt(data_word_width.W)))
+        val in_config_decoup = Wire(DecoupledIO(Bool()))
+
+        io_in_decoup.bits <> io.in.head.bits;   in_config_decoup.bits <> io.in.head.config
+        io_in_decoup.valid := (io.in.head.valid && (i.U === distribute_counter))
+        in_config_decoup.valid := (io.in.head.valid && (i.U === distribute_counter))
+        io_in_decoup.ready <> io.in.head.ready; in_config_decoup.ready <> io.in.head.ready
+
+        val bit_decoup = Queue(io_in_decoup,p.channel_buffer)
+        val config_decoup = Queue(in_config_decoup,p.channel_buffer)
+
+        io.out(i).bits <> bit_decoup.bits
+        io.out(i).config <> config_decoup.bits
+        io.out(i).valid <> bit_decoup.valid
+        io.out(i).valid <> config_decoup.valid
+        io.out(i).ready <> bit_decoup.ready
+        io.out(i).ready <> config_decoup.ready
+      }
     }else{
+      val vector_registers = Reg(Vec(num_vector,ReqAckConf_t(data_word_width)))
       for (i <- 0 until num_vector)
         vector_registers(i) <> io.in(i)
       io.out.head <> vector_registers(distribute_counter)
     }
-
     distribute_counter := distribute_counter + 1.U
   }else{
     io.out.head <> io.in.head
   }
+
+
 
   def get_port (io_t:String,name:String) = {
     io_t match {
