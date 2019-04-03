f137979608a3da6fa3ac00281c9dbc204fbdc171
SihaoLiu <liusihao.china@gmail.com>
2019-04-02 18:56:02 -0700

    update router IR (add DSE)

--- src/analytical_model/src/build_model/dse_util.py
+++ src/analytical_model/src/build_model/dse_util.py
@@ -5,12 +5,13 @@ def elaborate_dse(dse_option):
     dse_keys = [k for k, v in dse_option.items()]
     dse_options = [v for k, v in dse_option.items()]
     dse_space = itertools.product(*dse_options)
-    for design_point in dse_space:
-        print(design_point)
     return dse_keys, dse_space
 
 
-def update_design_point(ir, ir_f, design_demension, design_point):
+def update_design_point(ir_f_name, design_demension, design_point):
+    ir_f = open(ir_f_name, 'w')
     for dimension in design_demension:
         ir[dimension] = design_point[design_demension.index(dimension)]
+
+    ir_f.close()
     return
